abb82e4645852298e369271af1085a7c2c5f5990
houseofbits <kpudzens@gmail.com>
2020-01-25 12:54:11 +0200

    e

--- main.cpp
+++ main.cpp
@@ -15,30 +15,49 @@ IoPi* bus2 = 0;
 ServoPi* pwm = 0;
 
 #define PWM_RESOLUTION 4095
-#define LED_GAMMA_EXPONENT 0.7
-#define LED_FADE_DELAY_US 100
+#define LED_GAMMA_EXPONENT 0.8
+#define LED_FADE_DELAY_US 0
+#define LED_FADE_STEP 5
 
 #define A_LEDPWM_PIN 1
-#define B_LEDPWM_PIN 1
-#define SENSOR_INPUT_PIN 2
+#define B_LEDPWM_PIN 2
+#define SENSOR_INPUT_PIN 1
 
+//Screens - facing from electronic box, A-left, B-right
 //1) Screen side = A|B
 //2) Wavelength = UVA|UVB|UVC
 //3) Lamp position = TOP|BOTTOM|LEFT|RIGHT
-#define A_UVA_TOP_PIN 1
-#define A_UVB_TOP_PIN 2
-#define A_UVC_TOP_PIN 3
-#define A_UVA_BOTTOM_PIN 4
-#define A_UVB_LEFT_PIN 5
-#define A_UVB_RIGHT_PIN 6
-#define B_UVA_TOP_PIN 7
-#define B_UVB_TOP_PIN 8
-#define B_UVC_TOP_PIN 9
-#define B_UVA_BOTTOM_PIN 10
-#define B_UVB_LEFT_PIN 11
-#define B_UVB_RIGHT_PIN 12
+#define A_UVC_TOP_PIN 6
+#define A_UVB_TOP_PIN 4
+#define A_UVA_TOP_PIN 3
+#define B_UVB_TOP_PIN 14
+#define B_UVC_TOP_PIN 15
+#define B_UVA_TOP_PIN 16
+
+
+#define A_UVB_TOPLEFT_PIN 9
+#define A_UVB_BOTTOMLEFT_PIN 10
+#define A_UVA_BOTTOM_PIN 7
+#define A_UVB_BOTTOMRIGHT_PIN 5
+#define A_UVB_TOPRIGHT_PIN 8
+
+#define B_UVB_TOPLEFT_PIN 11
+#define B_UVA_BOTTOM_PIN 12
+#define B_UVB_BOTTOMRIGHT_PIN 2
+#define B_UVB_TOPRIGHT_PIN 1
 
 #define INACTIVITY_RESET_TIMEOUT 5    //Inactivity period (S) after which sequence starts over
+
+#define LED_LIGHT_ON_PERIOD 30
+#define UVA_LIGHT_ON_PERIOD 30
+#define UVB_LIGHT_ON_PERIOD 30
+#define UVC_LIGHT_ON_PERIOD 30
+#define UVALL_LIGHT_ON_PERIOD 30
+
+void Debug(std::string str)
+{
+//    cout<<str<<endl;
+}
 
 /**
  * @param delay time in seconds
@@ -78,8 +97,11 @@ float exponentialEasing (float x, float a)
  * */
 void ResetOutputs()
 {
-    bus1->write_port(0, 0x00);
-    bus2->write_port(1, 0x00);
+    bus1->write_port(0, 0xFF);
+    bus1->write_port(1, 0xFF);
+
+    if(pwm)pwm->set_pwm(1, 0, PWM_RESOLUTION, 0x40);
+    if(pwm)pwm->set_pwm(2, 0, PWM_RESOLUTION, 0x40);
 }
 
 /**
@@ -90,11 +112,18 @@ void InitIO()
     try{
 
         bus1 = new IoPi(0x20);
-        bus2 = new IoPi(0x21);      
+        bus1->set_port_direction(0, 0x00);
+        bus1->set_port_direction(1, 0x00);        
+        bus1->write_port(0, 0xFF);
+        bus1->write_port(1, 0xFF);
+
+        bus2 = new IoPi(0x21);
 
         bus1->set_port_direction(0, 0x00);
         bus1->set_port_direction(1, 0x00);
         bus2->set_port_direction(0, 0xFF);
+        bus2->set_pin_pullup(1,1);
+        bus2->set_pin_pullup(2,1);
 
         pwm = new ServoPi(0x40, 1);
         pwm->set_pwm_freq(300, 0x40);
@@ -113,10 +142,10 @@ void InitIO()
 /**
  * @param LED output pin number
  * */
-void FadeLEDIn(int pinId)
+void FadeLEDOff(int pinId)
 {
     float f = 0;
-    for (int x = 1; x <= PWM_RESOLUTION; x = x + 1) {
+    for (int x = 1; x <= PWM_RESOLUTION; x = x + LED_FADE_STEP) {
         usleep(LED_FADE_DELAY_US);
         f = exponentialEasing((float)x/(float)PWM_RESOLUTION, LED_GAMMA_EXPONENT);
         f = f * (float)PWM_RESOLUTION;
@@ -127,15 +156,31 @@ void FadeLEDIn(int pinId)
 /**
  * @param LED output pin number
  * */
-void FadeLEDOut(int pinId)
+void FadeLEDOn(int pinId)
 {
     float f = 0;
-    for (int x = 1; x <= PWM_RESOLUTION; x = x + 1) {
+    for (int x = 1; x <= PWM_RESOLUTION; x = x + LED_FADE_STEP) {
         usleep(LED_FADE_DELAY_US);
         f = exponentialEasing((float)x/(float)PWM_RESOLUTION, LED_GAMMA_EXPONENT);
-        f = f * (float)PWM_RESOLUTION;        
+        f = (1.0 - f) * (float)PWM_RESOLUTION;        
         if(pwm)pwm->set_pwm(pinId, 0, (int)f, 0x40);
     }
+}
+
+/**
+ * @param LED output pin number
+ * */
+void SetLEDOn(int pinId)
+{
+    if(pwm)pwm->set_pwm(pinId, 0, 0, 0x40);
+}
+
+/**
+ * @param LED output pin number
+ * */
+void SetLEDOff(int pinId)
+{
+    if(pwm)pwm->set_pwm(pinId, 0, PWM_RESOLUTION, 0x40);
 }
 
 /**
@@ -154,9 +199,9 @@ bool WaitForSensorActivation()
         int in = 1; 
         
         if(bus2)in = bus2->read_pin(SENSOR_INPUT_PIN);
-        
-        if (in) {
-            
+        else return true;       
+
+        if (in == 0) {            
             return true;
         }
 
@@ -178,14 +223,14 @@ bool WaitForSensorActivation()
  * @param output pin number
  * */
 void IOOn(int pin){
-    if(bus1)bus1->write_pin(pin, 1);
+    if(bus1)bus1->write_pin(pin, 0);
 }
 
 /**
  * @param output pin number
  * */
 void IOOff(int pin){
-    if(bus1)bus1->write_pin(pin, 0);
+    if(bus1)bus1->write_pin(pin, 1);
 }
 
 /**
@@ -247,51 +292,161 @@ void devTestOutputs(){
     }
 }
 
+void UVAOn(){
+    IOOn(A_UVA_BOTTOM_PIN);            
+    IOOn(B_UVA_BOTTOM_PIN);
+
+    Delay(1);
+
+    IOOn(A_UVA_TOP_PIN);            
+    IOOn(B_UVA_TOP_PIN);            
+}
+void UVAOff(){
+    Debug("UVA off");
+
+    IOOff(A_UVA_BOTTOM_PIN);            
+    IOOff(B_UVA_BOTTOM_PIN);            
+
+    Delay(1);
+
+    IOOff(A_UVA_TOP_PIN);            
+    IOOff(B_UVA_TOP_PIN);            
+}
+void UVBOn(){
+    Debug("UVB on");
+
+    IOOn(A_UVB_TOPLEFT_PIN);            
+    IOOn(A_UVB_TOPRIGHT_PIN);
+    IOOn(A_UVB_BOTTOMLEFT_PIN);            
+    IOOn(A_UVB_BOTTOMRIGHT_PIN);
+    IOOn(B_UVB_TOPLEFT_PIN);            
+    IOOn(B_UVB_TOPRIGHT_PIN);
+    IOOn(B_UVB_BOTTOMRIGHT_PIN);
+
+    Delay(1);
+
+    IOOn(A_UVB_TOP_PIN);            
+    IOOn(B_UVB_TOP_PIN);                        
+}
+void UVBOff(){
+    Debug("UVB off");
+
+    IOOff(A_UVB_TOPLEFT_PIN);            
+    IOOff(A_UVB_TOPRIGHT_PIN);
+    IOOff(A_UVB_BOTTOMLEFT_PIN);            
+    IOOff(A_UVB_BOTTOMRIGHT_PIN);
+    IOOff(B_UVB_TOPLEFT_PIN);            
+    IOOff(B_UVB_TOPRIGHT_PIN);
+    IOOff(B_UVB_BOTTOMRIGHT_PIN);
+
+    Delay(1);    
+
+    IOOff(A_UVB_TOP_PIN);            
+    IOOff(B_UVB_TOP_PIN);                        
+}
+void UVCOn(){
+    Debug("UVC on");
+
+    IOOn(A_UVC_TOP_PIN);     
+    IOOn(B_UVC_TOP_PIN);                        
+}
+void UVCOff(){
+    Debug("UVC off");
+
+    IOOff(A_UVC_TOP_PIN);            
+    IOOff(B_UVC_TOP_PIN);                        
+}
+
+
+
 int main() 
 {
-
-    //  float a = 0.7f;   
-    //  cout<<"0.1  "<<exponentialEasing(0.1,a)<<endl;     
-    //  cout<<"0.2  "<<exponentialEasing(0.2,a)<<endl;     
-    //  cout<<"0.35 "<<exponentialEasing(0.35,a)<<endl;          
-    //  cout<<"0.5  "<<exponentialEasing(0.5,a)<<endl;
-    //  cout<<"0.65 "<<exponentialEasing(0.65,a)<<endl;     
-    //  cout<<"0.8  "<<exponentialEasing(0.8,a)<<endl;
-    //  cout<<"0.9  "<<exponentialEasing(0.9,a)<<endl;     
-    //  return 0;
-
     InitIO();
 
-    devTestOutputs();
-
-    return 0;
+    //devTestOutputs();   return 0;
 
     while(true){
 
         try{
-
+            
+            ////////////////////////////////////////////////////////
+            // Fade in LED light
             if(!WaitForSensorActivation()) continue; //Start over from beginning
 
-            FadeLEDIn(A_LEDPWM_PIN);
-            FadeLEDIn(B_LEDPWM_PIN);
-        
-            Delay(5);
+            Debug("LED fade in");
 
-            IOOn(A_UVA_BOTTOM_PIN);
+            SetLEDOff(A_LEDPWM_PIN);
+            SetLEDOff(B_LEDPWM_PIN);
 
-            FadeLEDOut(A_LEDPWM_PIN);
-            FadeLEDOut(B_LEDPWM_PIN);  
+            FadeLEDOn(A_LEDPWM_PIN);
+            FadeLEDOn(B_LEDPWM_PIN);
 
+            Debug("LED delay");
+
+            //LED light delay period
+            Delay(LED_LIGHT_ON_PERIOD);
+
+            Debug("LED fade off");
+
+            //Fade off LED light
+            FadeLEDOff(A_LEDPWM_PIN);
+            FadeLEDOff(B_LEDPWM_PIN);  
+
+            ///////////////////////////////////////////////////////
+            //Switch on UVA light            
             if(!WaitForSensorActivation()) continue;
 
-            //Proceed to switch on UVA lamps
+            Debug("UVA on");
 
-            IOOn(A_UVA_BOTTOM_PIN);
+            UVAOn();
 
-            IOOn(A_UVA_TOP_PIN);
+            Debug("UVA delay");
 
-            //...
-        }
+            //UVA light delay period
+            Delay(UVA_LIGHT_ON_PERIOD);
+
+            UVAOff();
+
+            ///////////////////////////////////////////////////////
+            //Switch on UVB light            
+            if(!WaitForSensorActivation()) continue;
+
+            UVBOn();
+
+            Debug("UVB delay");
+
+            //UVB light delay period
+            Delay(UVB_LIGHT_ON_PERIOD);
+
+            UVBOff();
+
+            ///////////////////////////////////////////////////////
+            //Switch on UVC light            
+            if(!WaitForSensorActivation()) continue;
+
+            UVCOn();    
+
+            Debug("UVC delay");
+
+            //UVC light delay period
+            Delay(UVC_LIGHT_ON_PERIOD);
+            
+            ///////////////////////////////////////////////////////
+            //Switch on ALL UV light            
+
+            UVAOn();
+            UVBOn();
+            UVCOn();
+
+            Debug("UVALL delay");
+                
+            Delay(UVALL_LIGHT_ON_PERIOD);
+
+            UVCOff();
+            UVBOff();
+            UVAOff();
+
+        }   
         catch (exception &e) {
             cout <<"Process error: "<< e.what() << endl;
             break;
